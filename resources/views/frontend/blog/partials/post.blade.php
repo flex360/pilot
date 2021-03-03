d31fb0199f3bac0826d739d5d069a412f3d4823b
Colton Williams <clwilliams4545@gmail.com>
2021-03-03 11:40:40 -0600

    Updated base content pages and base post detail pages

--- resources/views/frontend/blog/partials/post.blade.php
+++ resources/views/frontend/blog/partials/post.blade.php
@@ -1,59 +1,156 @@
-@php
-    $detail = isset($detail) ? $detail : false;
-@endphp
+@extends('layouts.internal')
 
-<h1>{!! $detail ? $post->title : $post->link() !!}</h1>
+@section('content')
 
-<div class="post-byline">
-    {{ $post->published_on->format('l, F j, Y g:i a') }}
-</div>
 
-<div class="post-body">
+<div id="page-container" class="container bg-white p-10 md:p-20 rounded-none md:rounded-b-3xl mb-10 {{ $post->horizontal_featured_image != null ? 'pt-10' : '' }}">
 
-    @if ($post->hasImage())
+    @if ($post->horizontal_featured_image != null || $post->vertical_featured_image != null)
+    @section ('extra-top')
+    <!-- header image and overlay text container -->
+    <div class="relative">
 
-        <img src="{{ $post->image }}" class="post-image">
+        <!-- Featured Image Container --> 
+        <div id="page-featured-image" class="relative container">
+            <div class="absolute inset-0 rounded-none md:rounded-b-3xl rounded-t-none featured-image-linear-gradient" style="z-index: 10;"></div>
+            <div class="aspect-ratio-box relative rounded-none md:rounded-b-3xl rounded-t-none">
+                <picture>
+                    <source srcset="{{ $post->vertical_featured_image != null ? $post->vertical_featured_image : $post->horizontal_featured_image }} 800w" media="(max-width: 768px)">
+                    <source srcset="{{ $post->horizontal_featured_image != null ? $post->horizontal_featured_image : $post->vertical_featured_image }} 1000w">
+                    <x-img
+                        src="{{ $post->horizontal_featured_image }}"
+                        class="w-full rounded-none md:rounded-b-3xl rounded-t-none"
+                        loading="lazy"
+                        alt="Collection Name"
+                    ></x-img>
+                </picture>
+            </div>
+        </div>
 
+        <!-- Overlay info -->
+        <div class="relative md:absolute h-auto w-full md:w-3/4 lg:w-2/5 xl:w-1/2 position-overlay bg-primaryDark md:bg-transparent text-white">
+            <div class="text-center text-white px-1 lg:px-10 py-5 pt-0 md:pt-5">
+
+                <!-- tags & date -->
+                <div class="font-body text-sm text-white pt-2 tags" style="font-size: 14px;">
+                @if ($post->tags->isNotEmpty())
+                    @foreach ($post->tags as $tag)
+                        <a href="{{ route('blog.tagged', ['id' => $tag->id, 'slug' => Str::slug($tag->name)]) }}"
+                            class=" font-light hover:underline">{{ $tag->name }}</a>
+                        @if (!$loop->last)
+                        <span> | </span>
+                        @endif
+                    @endforeach
+                @endif
+                    <span class="ml-5">
+                        <time class="font-body text-white font-light text-sm mt-4 hidden md:inline">
+                            {{ $post->published_on->format('l, M j, Y') }}
+                        </time>
+                    </span>
+                </div>
+                
+                <div class="font-display text-xl lg:text-2xl xl:text-3xl w-full pt-2"><span class="">{{ ucwords($post->title) }}</span></div>
+                <p class="font-body font-light text-lg pt-2 px-5 md:px-0">{!! $post->getSummary() !!}</p>
+                <span class="ml-5">
+                    <time class="font-body text-white font-light text-sm mt-0 md:mt-4 block md:hidden">
+                        {{ $post->published_on->format('l, M j, Y') }}
+                    </time>
+                </span>
+            </div>
+        </div>
+    </div>
+
+        @endsection
+
+    
+
+    @else
+    <div class="py-32">
+        <h1 class="h1 mb-0">
+            {!! $post->title !!}
+        </h1>
+        <div class="font-body text-primaryNormal">
+            <span class="mr-3">{{ $post->published_on->format('l, M j, Y') }}</span>
+            <span>
+                <i class="fal fa-tag" style="transform: scaleX(-1);"></i>
+                @if ($post->tags->isNotEmpty())
+                    @foreach ($post->tags as $tag)
+                        <a href="{{ route('blog.tagged', ['id' => $tag->id, 'slug' => Str::slug($tag->name)]) }}"
+                            class="font-light hover:underline">{{ $tag->name }}</a>
+                        @if (!$loop->last)
+                        <span> | </span>
+                        @endif
+                    @endforeach
+                @endif
+            </span>
+
+            <div class="">{!! $post->getSummary() !!}</div>
+            
+        </div>
+    
     @endif
 
-    {!! $post->body !!}
+   <!-- body content -->
+   <div class="user-html mt-10">
+        {!! $post->body !!}
+    </div>
 
 </div>
 
-@if ($post->hasGallery())
+    @section('extra-bottom')
+    <!-- gallery -->
+    @if ($post->hasGallery())
+    <div class="gallery-container container px-5">
+        <div class="flex justify-between items-center w-full">
+            <h3 class="h3 text-complementaryDarkBrown">Image Gallery</h3>
+            <div onclick="toggleModal(0)" class="hidden md:block font-body text-primaryNormal hover:text-primaryLight transition-all ease-in-out duration-300 no-underline hover:no-underline text-lg mb-3 cursor-pointer">
+                <span><i class="far fa-chevron-right text-sm mr-2"></i> View all</span>
+            </div>
+        </div>
 
-    <div class="blog-slider">
+        <!-- grid of gallery images -->
+        <div class="flex flex-wrap -mx-3 mt-4">
+        @foreach($post->gallery as $index => $img)
+            <div onclick="toggleModal({{ $index }})" class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 px-3 rounded-xl cursor-pointer">
+                <div class="aspect-ratio-box relative rounded-3xl mb-8">
+                    <img src="{{ $img['path'] }}" alt="{{ $img['title'] }}" class="w-full rounded-3xl">
+                </div>
+            </div>
+            
+            <!-- galleryImage Modal -->
+            @include('partials.galleryImageModal', compact('index', 'img'))
+        @endforeach
+        </div>
+        
 
-        <ul>
+        <!-- view all link mobile -->
+        <div onclick="toggleModal(0)" class="flex justify-center block md:hidden font-body text-primaryNormal hover:text-primaryLight transition-all ease-in-out duration-300 no-underline hover:no-underline text-lg mb-3 cursor-pointer">
+            <span><i class="far fa-chevron-right text-sm mr-2"></i> View all</span>
+        </div>
+    </div>
+    @endif
 
-            @foreach ($post->gallery as $image)
-
-                <li style="background-image: url('{{ $image['path'] }}');">
-
-                    <h3>{{ $image['title'] or '' }}</h3>
-
-                    <p>{{ $image['caption'] or '' }}</p>
-
-                </li>
-
-            @endforeach
-
-        </ul>
-
+    <div class="{{ $post->hasGallery() ? 'pt-12' : 'pt-16' }}">
+        <hr class="border-primaryLight w-4/5 m-auto">
     </div>
 
-@endif
+    <div class="w-full flex justify-center py-20">
+        <button onclick="window.history.go(-1)" class="btn-danger text-lg px-10 py-5">
+            Return to News
+        </button>
+    </div>
+    @endsection
 
-<div class="post-tags">
-    Tags: {!! $post->present()->getTagLinks() !!}
+    @if ($post->horizontal_featured_image == null)
+    </div>
+    @endif
+
 </div>
 
-@if ($detail)
+@endsection
 
-    <div class="post-footer">
-
-        <a href="{{ route('blog') }}" class="btn btn-default"><i class="fa fa-arrow-left"></i> Back to News</a>
-
-    </div>
-
-@endif
+@push('scripts')
+    <script src="/pilot-assets/legacy/js/modal.js?v=3"></script>
+    <script src="/pilot-assets/legacy/js/modalArrowKey.js"></script>
+    
+@endpush
--- resources/views/partials/page.blade.php
+++ resources/views/partials/page.blade.php
@@ -1,53 +1,61 @@
-@if ($page->featured_image != null)
-    @section ('extra-top')
-        <!-- Featured Image Container --> 
-        <div class="relative">
-            <picture>
-                <source srcset="{{ $page->featured_image }} 800w" media="(max-width: 768px)">
-                <source srcset="{{ $page->featured_image }} 1000w">
-                <x-img
-                    src="{{ $page->featured_image }}"
-                    class="w-full"
-                    loading="lazy"
-                    alt="Collection Name"
-                ></x-img>
-            </picture>
 
-            <!-- Overlay info -->
-            <div class="absolute h-auto w-2/3 md:w-1/2 lg:w-2/5 xl:w-1/3 hidden md:inline" style="top: 35px; background: rgba(255, 255, 255, 0.75);">
-                <div class="px-10 py-5">
-                    {{-- <div class="w-full uppercase font-light"><span class="text-crail">Collection</span></div> --}}
-                    <div class="w-full text-3xl font-display mt-2 border-b border-solid border-crail uppercase"><span class="">{!! $page->title !!}</span></div>
-                    <p class="font-body pt-5">{{ $page->meta_description }}</p>
-                </div>
+<div id="page-container" class="container bg-white p-10 md:p-20 rounded-none md:rounded-b-3xl mb-10 {{ $page->featured_image != null ? 'pt-10' : '' }}">
+
+    @if ($page->featured_image != null || $page->vertical_featured_image != null)
+    @section ('extra-top')
+    <!-- header image and overlay text container -->
+    <div class="relative">
+
+        <!-- Featured Image Container --> 
+        <div id="page-featured-image" class="relative container">
+            <div class="absolute inset-0 rounded-none md:rounded-b-3xl rounded-t-none featured-image-linear-gradient" style="z-index: 10;"></div>
+
+            <div class="aspect-ratio-box relative rounded-none md:rounded-b-3xl rounded-t-none">
+                <picture>
+                    <source srcset="{{ $page->vertical_featured_image != null ? $page->vertical_featured_image : $page->featured_image }} 800w" media="(max-width: 768px)">
+                    <source srcset="{{ $page->featured_image != null ? $page->featured_image : $page->vertical_featured_image }} 1000w">
+                    <x-img
+                        src="{{ $page->featured_image }}"
+                        class="w-full rounded-3xl rounded-t-none"
+                        loading="lazy"
+                        alt="Collection Name"
+                    ></x-img>
+                </picture>
             </div>
-            <!-- Red line at the bottom of the Featured Image container -->
-            {{-- <div class="relative border-b border-solid border-crail mx-10" style="bottom: 30px"></div> --}}
         </div>
 
         <!-- Overlay info -->
-        <div class="w-full inline md:hidden" style="top: 35px; background: rgba(255, 255, 255, 0.75);">
-            <div class="px-10 py-5">
+        <div class="relative md:absolute h-auto w-full md:w-3/4 lg:w-2/5 xl:w-1/2 position-overlay-content-page bg-primaryDark md:bg-transparent text-white">
+            <div class="text-center text-white px-10 pb-10 lg:px-10 py-5">
                 {{-- <div class="w-full uppercase font-light"><span class="text-crail">Collection</span></div> --}}
-                <div class="w-full text-3xl font-display mt-2 border-b border-solid border-crail uppercase"><span class="">{!! $page->title !!}</span></div>
-                <p class="font-body pt-5">{{ $page->meta_description }}</p>
+                <div class="font-display text-xl lg:text-2xl xl:text-3xl w-full mt-2"><span class="">{!! $page->title !!}</span></div>
+                <p class="font-body font-light text-lg pt-2">{{ $page->meta_description }}</p>
             </div>
         </div>
-
+    </div>
+        
     @endsection
 
-@else
-<div class="py-5">
-    <div class="w-full uppercase text-lg font-light mt-12"><span class="text-crail"></span></div>
-    <div class="w-full text-3xl font-display font-bold mt-2 border-b border-solid border-crail"><span class="">{!! $page->title !!}</span></div>
-@endif
+    @else
+    <div class="pt-32 pb-8">
+        <h1 class="h1 text-3xl md:text-6xl">{{ $page->title }}</h1>
+        <p class="p">{{ $page->meta_description }}</p>
+    @endif
 
-{!! $page->block(1) !!}
+    {!! $page->block(1) !!}
 
-{!! $page->body !!}
+    {!! $page->body !!}
 
-{!! $page->block(2) !!}
+    {!! $page->block(2) !!}
 
-@if ($page->featured_image == null)
+    @if ($page->featured_image == null)
+    </div>
+    @endif
+
 </div>
-@endif
+
+</div>
+
+
+
+
