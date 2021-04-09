43249c1746d429aa7c1834722fcd8f6e03f6bb09
Colton Williams <clwilliams4545@gmail.com>
2021-04-09 14:50:12 -0500

    updated fix:media command with newest modules

--- src/Console/Commands/FixMedia.php
+++ src/Console/Commands/FixMedia.php
@@ -2,15 +2,24 @@
 
 namespace Flex360\Pilot\Console\Commands;
 
+use Flex360\Pilot\Pilot\Faq;
 use Flex360\Pilot\Pilot\Page;
 use Flex360\Pilot\Pilot\Post;
 use Flex360\Pilot\Pilot\Event;
 use Illuminate\Console\Command;
+use Flex360\Pilot\Pilot\Product;
+use Flex360\Pilot\Pilot\Project;
+use Flex360\Pilot\Pilot\Service;
 use Flex360\Pilot\Pilot\Employee;
 use Flex360\Pilot\Pilot\Resource;
 use Illuminate\Support\Facades\DB;
 use Flex360\Pilot\Pilot\Department;
 use Illuminate\Support\Facades\App;
+use Flex360\Pilot\Pilot\FaqCategory;
+use Flex360\Pilot\Pilot\Testimonial;
+use Flex360\Pilot\Pilot\ProductCategory;
+use Flex360\Pilot\Pilot\ProjectCategory;
+use Flex360\Pilot\Pilot\ServiceCategory;
 use Flex360\Pilot\Pilot\ResourceCategory;
 
 class FixMedia extends Command
@@ -54,6 +63,15 @@ class FixMedia extends Command
             Post::class,
             Resource::class,
             ResourceCategory::class,
+            Service::class,
+            ServiceCategory::class,
+            Product::class,
+            ProductCategory::class,
+            Faq::class,
+            FaqCategory::class,
+            Project::class,
+            ProjectCategory::class,
+            Testimonial::class,
         ];
 
         foreach ($coreClasses as $coreClass) {
