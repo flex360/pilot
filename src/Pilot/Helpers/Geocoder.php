476e2a979ec28eedbe55d60e88d585338db08e16
Colton Williams <clwilliams4545@gmail.com>
2021-10-14 14:49:45 -0500

    Updated Geocoder to handle more cases

--- src/Pilot/Helpers/Geocoder.php
+++ src/Pilot/Helpers/Geocoder.php
@@ -7,25 +7,31 @@ class Geocoder
     public static function fromAddress($address)
     {
         $params = array(
-            'key' => env('GOOGLE_API_MAPS_KEY'),
+            'key' => env('GOOGLE_API_GEOCODING_KEY'),
             'sensor' => 'false',
             'address' => urlencode($address),
         );
+
 
         $url = 'https://maps.googleapis.com/maps/api/geocode/json?' . http_build_query($params);
 
         $result = json_decode(file_get_contents($url));
 
-        $status = null;
-
-        if (isset($result->results) && isset($result->results[0])) {
-            $status = 'success';
+        if (isset($result->results) && !empty($result->results)) {
             $location = $result->results[0]->geometry->location;
         } else {
-            $status = 'failed';
-            $location = null;
+            $message = $result->error_message ?? 'Gecoding failed.';
 
-            return false;
+            if (isset($result->status) && $result->status == 'ZERO_RESULTS') {
+                $message = 'Address not found. Latitude and longitude not updated.';
+            }
+            
+            // throw(new \Exception($message));
+
+            return [
+                'status' => 'failed',
+                'message' => $message,
+            ];
         }
 
         return $location;
