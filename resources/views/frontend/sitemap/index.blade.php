0f8e6cb6c8341265106ff8a5fb902063e334f353
Colton Williams <clwilliams4545@gmail.com>
2021-10-18 10:08:14 -0500

    added classes to sitemap page

--- resources/views/frontend/sitemap/index.blade.php
+++ resources/views/frontend/sitemap/index.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
 
-	<ul>
-		<li><a href="{{ $root->url() }}">{{ $root->title }}</a></li>
+	<ul class="sitemap-ul ul">
+		<li class="sitemap-li "><a class="sitemap-a a" href="{{ $root->url() }}">{{ $root->title }}</a></li>
 		@foreach ($root->getChildren() as $child)
-			<li><a href="{{ $child->url() }}">{{ $child->title }}</a></li>
+			<li><a class="sitemap-a a" href="{{ $child->url() }}">{{ $child->title }}</a></li>
 		@endforeach
 	</ul>
 
