a963d9871642b49ed21e324fe7603c65b36f48ea
Colton Williams <clwilliams4545@gmail.com>
2021-10-20 15:24:07 -0500

    added if statement to sitemap to check if child item belongs on site map

--- resources/views/frontend/sitemap/index.blade.php
+++ resources/views/frontend/sitemap/index.blade.php
@@ -5,7 +5,9 @@
 	<ul class="sitemap-ul ul">
 		<li class="sitemap-li "><a class="sitemap-a a" href="{{ $root->url() }}">{{ $root->title }}</a></li>
 		@foreach ($root->getChildren() as $child)
+            @if($child->belongsOnSiteMap())
 			<li><a class="sitemap-a a" href="{{ $child->url() }}">{{ $child->title }}</a></li>
+            @endif
 		@endforeach
 	</ul>
 
